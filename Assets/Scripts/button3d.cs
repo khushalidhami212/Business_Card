b61c192ae96cc971b5ed60094c4c0e912a25618c
khushalidhami212 <19comp.khushali.dhami@gmail.com>
2022-03-25 18:42:11 +0530

    first commit

---
+++ Assets/Scripts/button3d.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button3d : MonoBehaviour
+{
+    public void mail()
+    {
+        Application.OpenURL("mailto: abc@gmail.com");
+    }
+    public void facebook()
+    {
+        Application.OpenURL("https://www.facebook.com/");
+    }
+    public void instagram()
+    {
+        Application.OpenURL("https://www.instagram.com/");
+    }
+    public void phone()
+    {
+        Application.OpenURL("tel: 1234567890");
+    }
+
+}
